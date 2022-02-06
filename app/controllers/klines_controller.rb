4e04f159750edbfdef4872af698874f4587fda07
Santiago <afroex.lucas@gmail.com>
2022-02-06 01:18:44 -0300

    feat: add Klines routes, view and controller

---
+++ app/controllers/klines_controller.rb
@@ -0,0 +1,10 @@
+class KlinesController < ApplicationController
+
+  def index
+    @data = FetchMovingAvgService.call(kline_params)
+  end
+
+  def kline_params
+    params.permit(:symbol, :interval, :type)
+  end
+end
